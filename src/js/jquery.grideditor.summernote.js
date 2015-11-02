cf8b7e1fad2cfee924ef5cd433898152adeaa19e
daslicht <daslicht@ansolas.de>
2015-11-02 15:49:17 +0100

    Added Summernote Integration

---
+++ src/js/jquery.grideditor.summernote.js
@@ -0,0 +1,56 @@
+(function() {
+
+    $.fn.gridEditor.RTEs.summernote = {
+
+        init: function(settings, contentAreas) {
+            
+            if (!jQuery().summernote) {
+                console.error('Summernote not available! Make sure you loaded the Summernote js file.');
+            }
+
+            var self = this;
+            contentAreas.each(function() {
+                var contentArea = $(this);
+                if (!contentArea.hasClass('active')) {
+                    if (contentArea.html() == self.initialContent) {
+                        contentArea.html('');
+                    }
+                    contentArea.addClass('active');
+
+                    var configuration = $.extend(
+                        (settings.summernote && settings.summernote.config ? settings.summernote.config : {}),
+                        {
+                            tabsize: 2,
+                            airMode: true,
+                            oninit: function(editor) {
+                                try {
+                                    settings.summernote.config.oninit(editor);
+                                } catch(e) {}
+                                $('#'+editor.settings.id).focus();
+                            }
+                        }
+                    );
+                    var summernote = contentArea.summernote(configuration);
+                }
+            });
+        },
+
+        deinit: function(settings, contentAreas) {
+            contentAreas.filter('.active').each(function() {
+                var contentArea = $(this);
+                var summernote = contentArea.tinymce();
+                if (summernote) {
+                    summernote.destroy()
+                }
+                contentArea
+                    .removeClass('active')
+                    .attr('id', null)
+                    .attr('style', null)
+                    .attr('spellcheck', null)
+                ;
+            });
+        },
+
+        initialContent: '<p>Lorem ipsum dolores</p>',
+    }
+})();
